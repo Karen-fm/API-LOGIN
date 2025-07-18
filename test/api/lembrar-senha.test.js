05f0cdb8a9cd15dfb621bf504d22ffacbdd1d4ce
brureis88 <brureis88@yahoo.com.br>
2025-07-18 09:28:51 -0300

    Remove node_modules do versionamento e adiciona ao .gitignore

---
+++ test/api/lembrar-senha.test.js
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import * as chai from 'chai'
+const expect = chai.expect
+import app from '../../index.js'
+
+describe('Lembrar Senha', () => {
+  it("deve retornar 404 quando o e-mail não existe", async () => {
+    const res = await request(app)
+      .post("/lembrar-senha")
+      .send({ email: "emailinvalido@email.com" })
+    expect(res.status).to.equal(404)
+    expect(res.body.message).to.equal("Email não encontrado")
+  })
+})
